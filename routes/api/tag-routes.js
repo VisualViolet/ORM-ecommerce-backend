9df61a68a215c7b4bd316cec9a4c239b1b530f51
VisualViolet <savannah_b_miller@outlook.com>
2023-02-04 19:16:53 -0800

    added routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,25 +4,94 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include:{
+      model: Product
+    }
+  }).then(data=>{
+    res.json(data)
+  }).catch(err=> {
+    res.status(500).json({
+      msg:"An error occured returning data.",
+      err: err
+    })
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include:{
+      model: Product
+    }
+  }).then(data=> {
+    if (data){
+      return res.json(data);
+    } else{
+      res.status(404).json({
+        msg:"Tag does not exist"
+      })
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({
+        msg:"An error occurred",
+        err:err
+    })
+  })
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name:req.body.tag_name
+  }).then(data=>{
+    res.status(201).json(data)
+  }).catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        msg:"An error occured",
+        err:err
+    })
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where:{
+        id:req.params.id
+    }
+  }).then(data=>{
+    if(data[0]){
+        return res.json(data)
+    } else {
+        return res.status(404).json({msg:"Category does not exist."})
+    }
+  }).catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        msg:"An error occurred",
+        err:err
+    })
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+        id:req.params.id
+    }
+  }).then(data=>{
+    if(data){
+        return res.json(data)
+    } else {
+        return res.status(404).json({msg:"Category does not exist."})
+    }
+  }).catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        msg:"An error occurred",
+        err:err
+    })
+  })
 });
 
 module.exports = router;
