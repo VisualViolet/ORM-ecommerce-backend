05df050b143d5b848d3903ebe9445730583602ab
VisualViolet <savannah_b_miller@outlook.com>
2023-02-04 16:23:55 -0800

    import sequelize connection and set up environmental variables

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+// Import packages
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection');
 
+// Set up Express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Handle data parsing with express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Sync sequelize models and turn on the server
+sequelize.sync({force:false}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
